5542eb9198c58fac1e477903a08b7b3a50590a88
Mohammedabdulrazighassan <mahammd_2010@hotmail.com>
2024-08-24 17:06:04 -0700

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,6 +7,6 @@ file_line { 'Turn off passwd auth':
 
 file_line { 'Declare identity file':
   ensure => present,
-  line   => '    IdentityFile ~/.ssh/school',
+  line   => '    IdentityFile ~/alx-system_engineering-devops/0x0B-ssh/school',
   path   => '/etc/ssh/ssh_config',
 }
